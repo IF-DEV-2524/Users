016730aec807bd5a47226e3b5b86de6cbcfea560
Igor Felipe <igorfelipe.dev@gmail.com>
2025-11-17 18:09:29 -0300

    Adicionadas algumas ferramentas como o prisma para facilitar as comunicações com o banco mongodb

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,15 +1,68 @@
 import express from 'express';
 
+// 1. Importe o dotenv
+import * as dotenv from 'dotenv'; 
+// 2. Chame a função config() logo no início
+dotenv.config();
+// Agora você pode importar e usar o PrismaClient
+import { PrismaClient } from '@prisma/client';
+
+// agr podemos ter acesso a todas as informações do banco de dados
+const prisma = new PrismaClient();
+
 const app = express();
 
-app.get('/usuarios', (req, res) => {
-    console.log('Requisição GET recebida em /usuarios');
-  res.send('Hello, World!');
+const user = [];
+
+app.use(express.json());
+
+app.post('/usuarios', async (req, res) => {
+    
+  // indica que o usuario foi criado com sucesso
+   if(res.status(201))
+    {
+      await prisma.user.create({
+                                data: {
+                                  name: 'Rich',
+                                  email: 'hello@prisma.com',
+                                  age: "30",
+                                  posts: {
+                                    create: {
+                                      title: 'My first post',
+                                    },
+                                  },
+                                },
+                              })
+
+      res.json(req.body);
+    }
+    else
+    {
+      res.send('Erro ao criar usuario');
+    }
 });
 
-app.post('/usuarios', (req, res) => {
-  res.send('Usuario creado');
+app.get('/usuarios', async (req, res) => {
+  // consultando usuarios
+  if(res.status(200))
+    {
+      
+       var allUsers = await prisma.user.findMany( {
+        include: {
+          posts: true,
+        },
+       });
+
+        res.json(allUsers);
+
+    }
+    else
+    {
+      res.send('Ocorreu uma falha ao buscar usuarios');
+    }
 });
+
+
 
 app.put('/usuarios/:id', (req, res) => {
   res.send(`Usuario con ID ${req.params.id} actualizado`);
@@ -20,3 +73,8 @@ app.delete('/usuarios/:id', (req, res) => {
 }); 
 
 app.listen(3000);
+
+/*
+  usuario: IgorDev
+  senha: IgorDev102030
+*/
